8b604468d5433aed85a21404a5c5e93347438f54
MOHAMMAD ASHFAQ <ashfaqsidd47@gmail.com>
2022-11-01 11:39:36 +0530

    socket.io server api created

---
+++ socket/index.js
@@ -0,0 +1,49 @@
+const { Socket } = require("socket.io");
+const cors = require("cors");
+
+const io = require("socket.io")(8900,{
+    cors:{
+        origin:"http://localhost:3000",
+    },
+});
+
+let users = [];
+
+const addUser = (userId,socketId)=>{
+    !users.some((user)=> user.userId === userId) &&
+        users.push({userId, socketId });
+}
+
+const getUser = (userId)=>{
+    return users.find(user=>user.userId === userId)
+}
+
+const removeUser = (socketId)=>{
+    users = users.filter(user=>user.socketId !== socketId);
+}
+
+io.on("connection", (socket) => {
+    //when any user is connected 
+    console.log("a user is connected..");
+    //take userId and socketId from user 
+    socket.on("addUser", userId=>{
+        addUser(userId, socket.id);
+        io.emit("getUsers", users);
+    });
+
+    //send a message 
+    socket.on("sendMessage", ({senderId, reciverId ,text})=>{
+        const user = getUser(reciverId);
+        io.to(user.socketId).emit("getMessage",{
+            senderId,
+            text,
+        });
+    });
+
+    //when a user is disconnected 
+    socket.on("disconnect", ()=>{
+        console.log("a user disconnected..");
+        removeUser(socket.id);
+        io.emit("getUsers", users);
+    })
+})
